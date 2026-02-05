41fce8300c23baaa367bd3c3ff25ae0f18666850
Fola Azeez <foladevops@gmail.com>
2026-02-05 10:04:59 +0000

    Update validate_values.py

--- automation/scripts/validate_values.py
+++ automation/scripts/validate_values.py
@@ -1,1 +1,19 @@
 
+import sys
+from lib.fail import fail
+from lib.loader import load_yaml
+from lib.validators import validate_environment
+
+
+def main() -> None:
+    try:
+        values = load_yaml("values.yaml")
+        validate_environment(values)
+        print("File validated successfully ✅")
+    except Exception as e:
+        print(f"ERROR: {e}")
+        sys.exit(1)
+
+
+if __name__ == "__main__":
+    main()
